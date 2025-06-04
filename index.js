1332c465c331689c1645b6cc55c4c70a225e0965
thej <thej.devops01@gmail.com>
2025-06-04 13:39:18 +0530

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js!');
+  res.send('Hello World from Node.js!.');
 });
 
 app.listen(PORT, () => {
